0c7caaa1f8a3182ab587f460636469be81730b7d
Ghannam <iia7mt@gmail.com>
2025-07-22 21:04:26 +0300

    refactor: assemble constants in one class

---
+++ Inventory Management System/Utils/Constants.cs
@@ -0,0 +1,11 @@
+namespace Inventory_Management_System.Utils;
+
+public static class Constants
+{
+    public const int AddProduct = 1;
+    public const int ViewAllProducts = 2;
+    public const int EditAProduct = 3;
+    public const int DeleteAProduct = 4;
+    public const int SearchAProduct = 5;
+    public const int CloseFlag = 6;
+}
