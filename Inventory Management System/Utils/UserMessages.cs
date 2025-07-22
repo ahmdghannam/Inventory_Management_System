5af17a39a40bc8cfc457485a3d29fe30a5e6ef26
Ghannam <iia7mt@gmail.com>
2025-07-22 16:39:18 +0300

    refactor: organize user messages

---
+++ Inventory Management System/Utils/UserMessages.cs
@@ -0,0 +1,35 @@
+namespace Inventory_Management_System.Utils;
+
+public class UserMessages
+{
+    public static void ShowGeneralErrorMessage()
+    {
+        Console.WriteLine("Invalid Input please try again !!");
+    }
+
+    public static void ShowOptionErrorMessage()
+    {
+        Console.WriteLine("Invalid number. please enter a valid option !!");
+    }
+
+    public static void ShowWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Store!");
+        Console.WriteLine("Here you can manage and the private store you own!");
+        Console.WriteLine("For help or any information contact me at info@Gh.com");
+        Console.WriteLine("Please choose one of the following options: ");
+        Console.WriteLine("1.Add a product: please write the product name, price, and quantity");
+        Console.WriteLine("2.View all products");
+        Console.WriteLine("3.Edit a product: please provide the name of the product");
+        Console.WriteLine("4.Delete a product: please provide the name of the product");
+        Console.WriteLine("5.Search for a product: please provide the name of the product");
+
+        Console.WriteLine("6.Exit");
+    }
+
+    public static void SecondaryWelcomeMessage()
+    {
+        Console.WriteLine("Welcome Again!");
+        Console.WriteLine("Please choose an operation number !");
+    }
+}
