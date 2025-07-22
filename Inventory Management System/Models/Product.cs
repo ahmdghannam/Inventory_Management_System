27e4d68f5813a7ea59d7aa0c928cac5ff8eb6885
Ghannam <iia7mt@gmail.com>
2025-07-22 15:46:11 +0300

    feat: add models for product and store

---
+++ Inventory Management System/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace Inventory_Management_System.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Inventory Management System/Models/Store.cs
@@ -0,0 +1,6 @@
+namespace Inventory_Management_System.Models;
+
+public class Store
+{
+    public List<Product> Products { get; set; }
+}
